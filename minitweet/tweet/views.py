0496a59eebbec57ef2fc6f96b0e92e0e104b2f68
Ronisha Shrestha <shrestharonisha374@gmail.com>
2024-12-01 21:43:56 +0545

    tweet backend initialization

--- minitweet/tweet/views.py
+++ minitweet/tweet/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 
 def index(request):
   return render(request, 'index.html')
+
+
+def tweet_list(request):
+  tweets = Tweet.objects.all().order_by('-created_at')
+  return render(request, 'tweet_list.html', {'tweets': tweets})
+
+def tweet_create(request):
+    if request.method == 'POST':
+      form = TweetForm(request.POST, request.FILES)
+      if form.is_valid():
+         tweet = form.save(commit = False)
+         tweet.user = request.user
+         return redirect('tweet_list')
+    else:
+       form = TweetForm()
+    return render(request, 'tweet_create.html', {'form': form})
+
+
