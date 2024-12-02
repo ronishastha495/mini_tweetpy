67226d33cead31cfb4bea31184ff54c1af89cea9
Ronisha Shrestha <shrestharonisha374@gmail.com>
2024-12-02 22:50:01 +0545

    completed all the views related work

--- minitweet/tweet/views.py
+++ minitweet/tweet/views.py
@@ -25,3 +25,23 @@ def tweet_create(request):
     return render(request, 'tweet_create.html', {'form': form})
 
 
+def tweet_edit(request, tweet_id):
+  tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
+  if request.method == 'POST':
+    form = TweetForm(request.POST, request.FILES, instance = twee)
+    if form.is_valid():
+      tweet = form.save(commit = False)
+      tweet.user = request.user
+      tweet.save()
+      return redirect('tweet-list')
+  else:
+    form = TweetForm(instance=tweet)
+  return render(request, 'tweet_form.html', {'form': form})
+
+
+  def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
+    if request.method == 'POST':
+      tweet.delete()
+      return redirect('tweet_list')
+    return render(request, 'tweet_confirm_delete.html', { 'form: form'})
